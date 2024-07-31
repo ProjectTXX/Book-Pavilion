c2411d639ad569cbeff7f31ae4443373052d3930
username <3506973472.qq.com>
2024-07-31 14:12:05 +0800

    THIRD

---
+++ widget.h
@@ -0,0 +1,30 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include<QString>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+
+
+private slots:
+    void on_Login_clicked();
+
+    void on_Enroll_clicked();
+
+private:
+    Ui::Widget *ui;
+};
+void Sql_init();//初始化数据库
+#endif // WIDGET_H
